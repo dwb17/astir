5a95fa748b6fcc28ca050abe21682f21d6730e1a
dwb17 <david@pointblank.gr>
2017-02-08 16:34:25 +0200

    Adding files

---
+++ src/assets/javascript/animate.js
@@ -0,0 +1,50 @@
+/**
+ * @file
+ * A JavaScript file for the waypoints fx.
+ *
+ * It uses the ccs-animate classes.
+ */
+(function ($) {
+  'use strict';
+  $(document).ready(function () {
+
+    // Hide all elements.
+    $('.js-waypoint-text').addClass('is-going-to-be-animated');
+    $('.js-waypoint-image').addClass('is-going-to-be-animated');
+
+    // Waypoint animate.
+    $('.js-waypoint-text').waypoint(function (direction) {
+      if (direction === 'down') {
+        // Show and animate elements once.
+        if ($(this.element).hasClass('is-going-to-be-animated')) {
+          $(this.element).removeClass('is-going-to-be-animated');
+          $(this.element).addClass('animated fadeInUp');
+        }
+      }
+      this.destroy();
+    }, {
+      offset: '70%',
+      triggerOnce: true,
+    });
+
+
+    $('.js-waypoint-image').waypoint(function (direction) {
+      if (direction === 'down') {
+        // Show and animate elements once.
+        if ($(this.element).hasClass('is-going-to-be-animated')) {
+          $(this.element).removeClass('is-going-to-be-animated');
+          // Check if parent has odd class to change direction
+          if($(this.element).parents('.section').hasClass('odd')) {
+            $(this.element).addClass('animated slideInRight');
+          } else {
+            $(this.element).addClass('animated slideInLeft');
+          }
+        }
+      }
+    }, {
+      offset: '80%'
+    });
+
+  });
+
+})(jQuery);
