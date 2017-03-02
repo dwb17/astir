a0d75bac595613efe565629b07523108dca968c2
dwb17 <david@pointblank.gr>
2017-03-02 12:48:52 +0200

    menu sticky

--- src/assets/javascript/animate.js
+++ src/assets/javascript/animate.js
@@ -7,11 +7,42 @@
 (function ($) {
   'use strict';
   $(document).ready(function () {
+    // Change menu icon color after video element.
+
+    $('#js-video').waypoint(function (direction) {
+      $('.hamburger').toggleClass('is-dark');
+    }, {
+      offset: '-80%',
+    });
+
+    $('.on-dark-color').each(function() {
+      new Waypoint.Inview({
+        element: this,
+        enter: function(direction) {
+          if(direction === 'up') {
+            $('.hamburger').removeClass('is-dark');
+          }
+        },
+        entered: function(direction) {
+          if(direction === 'up') {
+            $('.hamburger').addClass('is-dark');
+          };
+        },
+        exit: function(direction) {
+          if(direction === 'down') {
+            $('.hamburger').removeClass('is-dark');
+          }
+        },
+        exited: function(direction) {
+          if(direction === 'down') {
+            $('.hamburger').addClass('is-dark');
+          }
+        }
+      })
+    });
+
     // Only add efects to desktop
     if(!window.matchMedia('(max-width: 768px)').matches) {
-
-
-
       // Hide all elements.
       $('.js-waypoint-text').addClass('is-going-to-be-animated');
       $('.js-waypoint-image').addClass('is-going-to-be-animated');
@@ -25,7 +56,7 @@
             $(this.element).addClass('animated fadeInUp');
           }
         }
-        this.destroy();
+        //this.destroy();
       }, {
         offset: '40%',
         triggerOnce: true,
